ac0e12de44d065c2fdc10f533c5b59d81751cc02
funnyboy-roks <fbr@fbr.dev>
2026-07-02 17:14:39 -0500

    split game state

---
+++ src/lib/game.ts
@@ -0,0 +1,110 @@
+import { Deck } from "./deck";
+import { Player, type PlayerAction } from "./player.svelte";
+import type { WinState } from "./types.svelte";
+import { sleep } from "./utils";
+
+// NOTE: this is not a class so we have full reactivity
+export interface GameState {
+    dealer: Player;
+    players: Player[];
+    deck: Deck;
+    stage: 'new' | 'play' | 'score';
+    turn: 'dealer' | number;
+}
+
+const dealerTurn = async (game: GameState) => {
+    while (true) {
+        const value = game.dealer.finalValue();
+        if (value === 'bust' || value > 17) {
+            break;
+        }
+        await sleep(100);
+        game.dealer.hand.push(game.deck.takeCard()!);
+    }
+    game.stage = 'score';
+};
+
+const nextTurn = (game: GameState) => {
+    // if we are on the last player, go to the dealer
+    if (game.turn === game.players.length - 1) {
+        game.turn = 'dealer';
+        dealerTurn(game);
+        return;
+    }
+
+    // reset calls nextTurn in this state.  It shouldn't happen in any other case
+    if (game.turn === 'dealer') {
+        game.turn = 0;
+    } else {
+        // otherwise, just add one
+        game.turn += 1;
+    }
+
+    // if the new player has blackjack, then we move on
+    if (game.players[game.turn].hasBlackjack()) {
+        nextTurn(game);
+    }
+};
+
+/**
+ * Reset the game back to a clean state, removing and re-dealing cards
+ */
+export const resetGame = (game: GameState) => {
+    game.deck = new Deck(2);
+    game.dealer = new Player('Dealer');
+    for (const player of game.players) {
+        player.hand = [];
+    }
+    game.turn = 'dealer';
+    game.stage = 'play';
+
+    game.deck.shuffle();
+
+    // deal cards in a circle (dealer -> players -> dealer -> players)
+    game.dealer.hand.push(game.deck.takeCard()!);
+    for (const player of game.players) {
+        player.hand.push(game.deck.takeCard()!);
+    }
+    game.dealer.hand.push(game.deck.takeCard()!);
+    for (const player of game.players) {
+        player.hand.push(game.deck.takeCard()!);
+    }
+
+    // switch to player 0
+    nextTurn(game);
+};
+
+export const onPlayerAction = (game: GameState, action: PlayerAction, playerIndex: number) => {
+    const player = game.players[playerIndex];
+    switch (action) {
+        case 'hit': {
+            player.hand.push(game.deck.takeCard()!);
+            if (player.hasBusted()) {
+                nextTurn(game);
+            }
+        }; break;
+        case 'stay': {
+            nextTurn(game);
+        }; break;
+        case 'double-down': {
+            player.hand.push(game.deck.takeCard()!);
+            // TODO: bets
+            nextTurn(game);
+        }; break;
+    }
+};
+
+export const getPlayerWinState = (game: GameState, player: Player): WinState => {
+    if (player.hasBlackjack()) return 'blackjack';
+
+    if (player.hasBusted()) return 'lose';
+    if (game.dealer.hasBusted()) return 'win';
+
+    // if the game is still running, then we don't know what the final state is
+    if (game.stage !== 'score') return undefined;
+
+    if (game.dealer.finalValue() === player.finalValue()) return 'push';
+
+    return player.finalValue() > game.dealer.finalValue() ? 'win' : 'lose';
+};
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,158 +1,61 @@
 <script lang="ts">
-	import NewGame from "$lib/components/NewGame.svelte";
-	import * as Card from '$lib/components/ui/card';
+	import NewGame from '$lib/components/NewGame.svelte';
 
-	import { type WinState } from "$lib/types.svelte";
     import { Deck } from '$lib/deck';
-	import PlayerCard from "$lib/components/PlayerCard.svelte";
-	import { onMount } from "svelte";
-	import { Button } from "$lib/components/ui/button";
-	import { Player, type PlayerAction } from "$lib/player.svelte";
+	import PlayerCard from '$lib/components/PlayerCard.svelte';
+	import { onMount } from 'svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { Player } from '$lib/player.svelte';
+	import { getPlayerWinState, onPlayerAction, resetGame, type GameState } from '$lib/game';
 
     let playerNames = $state<string[]>(['foo', 'bar']);
 
-    // TODO: use pages for this
-    let stage: 'new' | 'play' | 'score' = $state('new');
-
-    let dealer = $state<Player>(new Player('Dealer'));
-    let players = $state<Player[]>([]);
-    let deck = $state<Deck>(new Deck(2));
-    let turn = $state<'dealer' | number>(0);
-
-    const sleep = (millis: number) => new Promise<void>(res => setTimeout(() => res(), millis));
+    const game = $state<GameState>({
+        stage: 'new',
+        dealer: new Player('Dealer'),
+        players: [],
+        deck: new Deck(2),
+        turn: 'dealer',
+    });
 
     const onstart = () => {
-        players = playerNames.map(name => new Player(name));
-        stage = 'play';
+        game.players = playerNames.map(name => new Player(name));
+        game.stage = 'play';
 
-        reset();
+        resetGame(game);
     };
 
-    const dealerTurn = async () => {
-        while (true) {
-            const value = dealer.finalValue();
-            if (value === 'bust' || value > 17) {
-                break;
-            }
-            await sleep(100);
-            dealer.hand.push(deck.takeCard()!);
-        }
-        stage = 'score';
-    };
-
-    const nextTurn = () => {
-        // if we are on the last player, go to the dealer
-        if (turn === players.length - 1) {
-            turn = 'dealer';
-            dealerTurn();
-            return;
-        }
-
-        // reset calls nextTurn in this state.  It shouldn't happen in any other case
-        if (turn === 'dealer') {
-            turn = 0;
-        } else {
-            // otherwise, just add one
-            turn += 1;
-        }
-
-        // if the new player has blackjack, then we move on
-        if (players[turn].hasBlackjack()) {
-            nextTurn();
-        }
-    };
-
-    const onPlayerAction = (action: PlayerAction, playerIndex: number) => {
-        const player = players[playerIndex];
-        switch (action) {
-            case 'hit': {
-                player.hand.push(deck.takeCard()!);
-                if (player.hasBusted()) {
-                    nextTurn();
-                }
-            }; break;
-            case 'stay': {
-                nextTurn();
-            }; break;
-            case 'double-down': {
-                player.hand.push(deck.takeCard()!);
-                // TODO: bets
-                nextTurn();
-            }; break;
-        }
-    };
-
-    const getPlayerWinState = (player: Player): WinState => {
-        if (player.hasBlackjack()) return 'blackjack';
-
-        if (player.hasBusted()) return 'lose';
-        if (dealer.hasBusted()) return 'win';
-
-        // if the game is still running, then we don't know what the final state is
-        if (stage !== 'score') return undefined;
-
-        if (dealer.finalValue() === player.finalValue()) return 'push';
-
-        return player.finalValue() > dealer.finalValue() ? 'win' : 'lose';
-    };
-
-    const reset = () => {
-        deck = new Deck(2);
-        dealer = new Player('Dealer');
-        for (const player of players) {
-            player.hand = [];
-        }
-        turn = 'dealer';
-        stage = 'play';
-
-        deck.shuffle();
-
-        // deal cards in a circle (dealer -> players -> dealer -> players)
-        dealer.hand.push(deck.takeCard()!);
-        for (const player of players) {
-            player.hand.push(deck.takeCard()!);
-        }
-        dealer.hand.push(deck.takeCard()!);
-        for (const player of players) {
-            player.hand.push(deck.takeCard()!);
-        }
-
-        // switch to player 0
-        nextTurn();
-    };
-
-    onMount(() => {
-        onstart();
-    });
+    // TODO: Remove this (testing)
+    onMount(onstart);
 </script>
 
 <div class="w-screen h-screen flex flex-col justify-center items-center gap-6 py-6">
     <h1 class="text-6xl">Blackjack</h1>
-    {#if stage === 'new'}
+    {#if game.stage === 'new'}
         <NewGame bind:players={playerNames} onstart={onstart} />
-    {:else if stage === 'play' || stage === 'score'}
+    {:else if game.stage === 'play' || game.stage === 'score'}
         <div class="grow w-full flex flex-col gap-4 items-center">
             <div>
-                {(turn === 'dealer' ? dealer : players[turn]).name}'s turn 
+                {(game.turn === 'dealer' ? game.dealer : game.players[game.turn]).name}'s turn 
             </div>
             <PlayerCard
-                player={dealer}
+                player={game.dealer}
                 isDealer
-                turn={turn === 'dealer'}
+                turn={game.turn === 'dealer'}
                 win={undefined}
             />
             <div class="flex flex-row gap-4 w-full justify-center">
-                {#each players as player, i (i)}
+                {#each game.players as player, i (i)}
                     <PlayerCard
                         {player}
-                        turn={turn === i}
-                        onaction={action => onPlayerAction(action, i)}
-                        win={getPlayerWinState(player)}
+                        turn={game.turn === i}
+                        onaction={action => onPlayerAction(game, action, i)}
+                        win={getPlayerWinState(game, player)}
                     />
-                {/each}
+               {/each}
             </div>
-            {#if stage === 'score'}
-                <Button onclick={reset}>Reset</Button>
+            {#if game.stage === 'score'}
+                <Button onclick={() => resetGame(game)}>Reset</Button>
             {/if}
         </div>
     {/if}
